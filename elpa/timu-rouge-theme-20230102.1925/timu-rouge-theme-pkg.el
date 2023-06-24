451458f505f5f62437eb5812b791590ab4907629
lenart <lenartlola@proton.me>
2023-06-24 17:29:55 +0200

    Add elpa packages

---
+++ elpa/timu-rouge-theme-20230102.1925/timu-rouge-theme-pkg.el
@@ -0,0 +1,14 @@
+(define-package "timu-rouge-theme" "20230102.1925" "Color theme inspired by the Rouge Theme for VSCode"
+  '((emacs "27.1"))
+  :commit "87117f15ea5fcfacbbad23ea6f345d18c2ff0009" :authors
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainers
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainer
+  '("Aimé Bertrand" . "aime.bertrand@macowners.club")
+  :keywords
+  '("faces" "themes")
+  :url "https://gitlab.com/aimebertrand/timu-rouge-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
