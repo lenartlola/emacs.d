ddef2dd2302791472ccd9a3c9bde1142b18c7bae
lenart <lenartlola@proton.me>
2023-06-25 14:41:04 +0200

    Set CC mode

--- init.el
+++ init.el
@@ -38,9 +38,11 @@
 (elcord-mode)
 
 ; Tab settings
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+(setq c-default-style '((java-mode . "java")
+                        (awk-mode . "awk")
+                        (other . "linux")))
+
+
 
 ; projectile
 (projectile-mode +1)
