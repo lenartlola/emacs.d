506c7be59c33023dcc2cde514730972d4dd6da33
lenart <lenartlola@proton.me>
2023-07-05 12:18:51 +0200

    Projectile codesearch

---
+++ elpa/projectile-codesearch-20180508.1522/projectile-codesearch-pkg.el
@@ -0,0 +1,15 @@
+(define-package "projectile-codesearch" "20180508.1522" "Integration of codesearch into projectile"
+  '((codesearch "20171122.431")
+    (projectile "20150405.126"))
+  :commit "e40efc62e9333db0593bd81b5c78d08b19bfb193" :authors
+  '(("Austin Bingham" . "austin.bingham@gmail.com"))
+  :maintainers
+  '(("Austin Bingham" . "austin.bingham@gmail.com"))
+  :maintainer
+  '("Austin Bingham" . "austin.bingham@gmail.com")
+  :keywords
+  '("tools" "development" "search")
+  :url "https://github.com/abingham/emacs-codesearch")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
